81b8024cab8fc368385f1f8d67faec9b75e389f7
Jason G <jason@jroid.com>
2019-01-31 21:16:57 -0500

    add locking function and username to selections

--- common/models/selection.js
+++ common/models/selection.js
@@ -20,12 +20,33 @@ module.exports = function(Selection) {
   Selection.observe('before save', function limitToUser(ctx, next) {
     var accessToken = ctx.options && ctx.options.accessToken;
     var userId = accessToken && accessToken.userId;
-
-    if (!userId) {
-      // without connected user. proceed without hook
-      return next();
-    }
-    ctx.instance.user_id = userId;
-    next();
+    const globals = ctx.Model.app.models.global;
+    globals.findOne({where: {setting: 'isLocked'}}, function(err, isLocked) {
+      if (err) return next(err);
+      let username = 'anonymous';
+      if (isLocked.value == true) {
+        err = new Error("Submissions are locked by the administrator");
+        err.statusCode = 400;
+        next(err);
+      } else {
+        const User = ctx.Model.app.models.User;
+        // ToDo: This should be moved to a better location
+        User.findById(accessToken.userId, function(err, user) {
+          if (err) return next(err);
+          username = user.username;
+          if (!userId) {
+            // without connected user. proceed without hook        
+            err = new Error("Submissions require a valid user account");
+            err.statusCode = 400;
+            next(err);
+            return next();
+          } else {
+            ctx.instance.user_id = userId;
+            ctx.instance.username = username;
+            next();
+          }
+        });
+      }
+    });
   });
 };
